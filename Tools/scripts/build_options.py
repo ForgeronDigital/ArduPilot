e513cff39318c9f79fd286137af2dcff899b982e
Nick Exton <NExton@sypaq.com.au>
2024-08-09 09:55:23 +1000

    Tools: Add CAMERA_INFO_FROM_SCRIPT guard to build_options.py

--- Tools/scripts/build_options.py
+++ Tools/scripts/build_options.py
@@ -141,6 +141,7 @@ BUILD_OPTIONS = [
     Feature('Camera', 'Camera_Solo', 'AP_CAMERA_SOLOGIMBAL_ENABLED', 'Enable Solo Gimbal support', 0, 'Camera'),
     Feature('Camera', 'Camera_FOV_Status', 'AP_CAMERA_SEND_FOV_STATUS_ENABLED', 'Enable Camera FOV Status send to GCS', 0, 'Camera,MOUNT'),  # noqa: E501
     Feature('Camera', 'Camera_ThermalRange', 'AP_CAMERA_SEND_THERMAL_RANGE_ENABLED', 'Enable Camera Thermal Range send to GCS', 0, 'Camera,MOUNT'),  # noqa: E501
+    Feature('Camera', 'Camera_Info_From_Script', 'AP_CAMERA_INFO_FROM_SCRIPT_ENABLED', 'Enable setting Camera information messages from Lua script', 0, 'Camera,SCRIPTING'), # noqa
 
     Feature('Camera', 'RUNCAM', 'HAL_RUNCAM_ENABLED', 'Enable RunCam Control', 0, None),
 
